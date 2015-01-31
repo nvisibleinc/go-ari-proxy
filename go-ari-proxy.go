b55fc7b17d0555b6cd7c803663573815117bfeac
blitzrage <blitzrage@n-visibleinc.com>
2015-01-31 10:00:00 -0500

    Latest round of changes

--- go-ari-proxy.go
+++ go-ari-proxy.go
@@ -13,81 +13,25 @@ import (
 	"syscall"
 	"strings"
 	"time"
-
 	"code.google.com/p/go.net/websocket"
 	"go-ari-library"
 )
 
 // Var config contains a Config struct to hold the proxy configuration file.
 var (
-	config Config
-	client = &http.Client{}
-	proxyInstances map[string] *proxyInstance
+	config Config						// main proxy configuration structure
+	client = &http.Client{}				// connection for Commands to ARI
+	proxyInstances *proxyInstanceMap	// maps the per-dialog proxy instances
 )
 
-// Struct Config holds the configuration for the proxy.
-// The Config struct contains the information the was unmarshaled from the
-// configuration file for ths proxy.
-type Config struct {
-	Origin        string      `json:"origin"`		// connection to ARI events
-	ServerID      string      `json:"server_id"`	// unique server ident
-	Applications  []string    `json:"applications"`	// slice of applications to listen for
-	Websocket_URL string      `json:"websocket_url"`// websocket to connect to
-	Stasis_URL    string      `json:"stasis_url"`   // Base URL of ARI REST API
-	WS_User       string      `json:"ws_user"`		// username of websocket connection
-	WS_Password   string      `json:"ws_password"`	// pass of websocket connection
-	MessageBus    string      `json:"message_bus"`	// type of message bus to publish to
-	BusConfig     interface{} `json:"bus_config"`	// configuration of the message bus we're publishing to
-}
-
-// ProxyInstance struct contains the channels necessary for communications
-// to/from the various message bus topics and the event channel. This is
-// primarily used as the communications bus for setting up new instances of
-// applications.
-type proxyInstance struct {
-	commandChannel		chan []byte
-	responseChannel		chan []byte
-	Events				chan []byte
-	quit				chan int
-	ariObjects			[]string
-}
-
-// eventInfo struct contains the information about an event that comes in.
-// Information about the event that we need to make a determination on the proxy side.
-// Track information associated with a given application instance.
-type eventInfo struct {
-	Type      string	`json:"type"`		// event type
-	Application string	`json:"application"`
-	Bridge    minBridge `json:"bridge"`		// bridge ID
-	Channel   minChan   `json:"channel"`	// channel ID
-	Recording minRec	`json:"recording"`	// recording name	TODO: why no id?
-	Playback  minPlay	`json:"playback"`	// playback ID
-}
-
-// minPlay struct is a minimal struct that contains the Playback ID.
-type minPlay struct {
-	ID	string	`json:"id"`
-}
-
-// minRec struct is a minimal struct that contains the Recording Name (no ID).
-type minRec struct {
-	Name	string	`json:"name"`
-}
-
-// minBridge struct is used to get the ID of a bridge in an event.
-type minBridge struct {
-	ID	string	`json:"id"`
-}
-
-// minChan struct is used to get the ID of a channel in an event.
-type minChan struct {
-	ID	string	`json:"id"`
-}
-
-// ID struct is used to get the ID or name of objects returned from ARI REST calls.
-type ID struct {
-	ID		string `json:"id"`
-	Name	string `json:"name"`
+// signalCatcher is a function to allows us to stop the application through an
+// operating system signal.
+func signalCatcher() {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT)
+	sig := <-ch
+	log.Printf("Signal received: %v", sig)
+	os.Exit(0)
 }
 
 // Init parses the configuration file by unmarshaling it into a Config struct.
@@ -105,7 +49,46 @@ func init() {
 	// read in the configuration file and unmarshal the json, storing it in 'config'
 	json.Unmarshal(configfile, &config)
 	fmt.Println(&config)
-	proxyInstances = make(map[string] *proxyInstance)
+	proxyInstances = NewproxyInstanceMap()		// initialize a new proxy instance map
+}
+
+func main() {
+	// Setup a new Event producer and Command consumer for every application
+	// we've configured in the configuration file.
+	ari.InitBus(config.MessageBus, config.BusConfig)
+	for _, app := range config.Applications {
+		/*
+			Create a new producer which is responsible for the initial topic on the message bus which is used
+			to signal the setup of per-application instances. All applications listen to this topic in order to
+			be provided the information to setup the ownership of per dialog application instances.
+		*/
+		producer := ari.InitProducer(app)	// Initialize a new producer channel using the ari.IntProducer function.
+		go runEventHandler(app, producer)	// create new websocket connection for every application and pass the producer channel
+	}
+
+	go signalCatcher()	// listen for os signal to stop the application
+	select {}
+}
+
+// runEventHandler sets up a websocket connection to an ARI application.
+func runEventHandler(s string, producer chan []byte) {
+	// Connect to the websocket backend (ARI)
+	var ariMessage string
+	url := strings.Join([]string{config.Websocket_URL, "?app=", s, "&api_key=", config.WS_User, ":", config.WS_Password}, "")
+	ws, err := websocket.Dial(url, "ari", config.Origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start the producer loop. Every message received from the websocket is
+	// passed to the PublishMessage() function.
+	for {
+		err = websocket.Message.Receive(ws, &ariMessage)		// accept the message from the websocket
+		if err != nil {
+			log.Fatal(err)
+		}
+		go PublishMessage(ariMessage, producer)	// publish message to the producer channel
+	}
 }
 
 // PublishMessage takes an ARI event from the websocket and places it on the
@@ -131,33 +114,38 @@ func PublishMessage(ariMessage string, producer chan []byte) {
 		fmt.Println("Dialog ID is:", dialogID)
 		as, err := json.Marshal(ari.AppStart{Application: info.Application, DialogID: dialogID})
 		producer <- as
+
+		// TODO: this sleep is required to allow the application time to spin up. In the future we likely want
+		// to implement some sort of feedback mechanism in order to remove this sleep timer.
 		time.Sleep(50 * time.Millisecond)
 		if err != nil {
 			return
 		}
-		pi = initProxyInstance(dialogID)
-		proxyInstances[info.Channel.ID] = pi
+		pi = NewProxyInstance(dialogID)				// create new proxy instance for the dialog
+		proxyInstances.Add(info.Channel.ID, pi)		// add the dialog to the proxyInstances map to track its life
 		exists = true
 	case info.Type == "StasisEnd":
 		// on application end, perform clean up checks
-		pi, exists = proxyInstances[info.Channel.ID]
+		pi, exists = proxyInstances.Get(info.Channel.ID)
 		if exists {
-			//pi.removeAllObjects()
+			pi.removeAllObjects()
 		}
 	case info.Type == "BridgeDestroyed":
-		pi, exists = proxyInstances[info.Bridge.ID]
+		pi, exists = proxyInstances.Get(info.Bridge.ID)
 		if exists {
 			pi.removeObject(info.Bridge.ID)
 		}
 	case info.Type == "ChannelDestroyed":
-		pi, exists = proxyInstances[info.Channel.ID]
+		pi, exists = proxyInstances.Get(info.Channel.ID)
 		if exists {
 			pi.removeObject(info.Channel.ID)
 		}
 	case strings.HasPrefix(info.Type, "Channel"):
-		pi, exists = proxyInstances[info.Channel.ID]
+		pi, exists = proxyInstances.Get(info.Channel.ID)
 	case strings.HasPrefix(message.Type, "Bridge"):
-		pi, exists = proxyInstances[info.Bridge.ID]
+		pi, exists = proxyInstances.Get(info.Bridge.ID)
+	case strings.HasPrefix(message.Type, "Playback"):
+		pi, exists = proxyInstances.Get(info.Playback.ID)
 	default:
 		fmt.Println("No handler for event type")
 		//pi, exists = proxyInstances[]
@@ -178,34 +166,30 @@ func PublishMessage(ariMessage string, producer chan []byte) {
 	}
 }
 
-// initProxyInstance initializes a new proxy instance.
-func initProxyInstance(dialogID string) *proxyInstance {
-	var p proxyInstance
-	p.quit = make(chan int)
-	p.Events = ari.InitProducer(strings.Join([]string{"events", dialogID}, "_"))
-	go p.runCommandConsumer(dialogID)
-	return &p
+// Add inserts a proxy instance into the global map of active proxy instances.
+func (p *proxyInstanceMap) Add(id string, pi *proxyInstance) {
+	p.mapLock.Lock()
+	defer p.mapLock.Unlock()
+	p.instanceMap[id] = pi
 }
 
-// runEventHandler sets up a websocket connection to an ARI application.
-func runEventHandler(s string, producer chan []byte) {
-	// Connect to the websocket backend (ARI)
-	var ariMessage string
-	url := strings.Join([]string{config.Websocket_URL, "?app=", s, "&api_key=", config.WS_User, ":", config.WS_Password}, "")
-	ws, err := websocket.Dial(url, "ari", config.Origin)
-	if err != nil {
-		log.Fatal(err)
+// Get returns a proxy instance from the global map of active proxy instances.
+// returns nil if not found.
+func (p *proxyInstanceMap) Get(id string) (*proxyInstance, bool) {
+	p.mapLock.RLock()
+	defer p.mapLock.RUnlock()
+	pi, ok := p.instanceMap[id]
+	if !ok {
+		return nil, false
 	}
+	return pi, true
+}
 
-	// Start the producer loop. Every message received from the websocket is
-	// passed to the PublishMessage() function.
-	for {
-		err = websocket.Message.Receive(ws, &ariMessage)		// accept the message from the websocket
-		if err != nil {
-			log.Fatal(err)
-		}
-		go PublishMessage(ariMessage, producer)	// publish message to the producer channel
-	}
+// Remove deletes an entry in the global proxyInstanceMap
+func (p *proxyInstanceMap) Remove(id string) {
+	p.mapLock.Lock()
+	defer p.mapLock.Unlock()
+	delete(p.instanceMap, id)
 }
 
 // shutDown closes the quit channel to signal all of a ProxyInstance's goroutines
@@ -216,15 +200,16 @@ func (p *proxyInstance) shutDown () {
 
 func (p *proxyInstance) addObject(id string) {
 	for i:= range p.ariObjects {
-		if p.ariObjects [i] == id {
+		if p.ariObjects[i] == id {
 			//object already is associated with this proxyInstance
 			return
 		}
 	}
 	p.ariObjects = append(p.ariObjects, id)
-	proxyInstances[id] = p
-	
+	proxyInstances.Add(id, p)
+
 }
+
 func (p *proxyInstance) removeObject(id string) {
 	// remove an object from the map.
 	for i := range p.ariObjects {
@@ -236,7 +221,7 @@ func (p *proxyInstance) removeObject(id string) {
 	}
 
 	// remove the instance from our tracking map
-	delete(proxyInstances, id)
+	proxyInstances.Remove(id)
 
 	// if there are no more objects, shut'rdown
 	if len(p.ariObjects) == 0 {
@@ -247,7 +232,7 @@ func (p *proxyInstance) removeObject(id string) {
 func (p *proxyInstance) removeAllObjects() {
 	// remove all objects from the map as our application is shutting down.
 	for _, obj := range p.ariObjects {
-		delete(proxyInstances, obj)
+		proxyInstances.Remove(obj)
 	}
 	p.shutDown()	// destroy the application / proxy instance
 }
@@ -266,7 +251,7 @@ func (p *proxyInstance) runCommandConsumer(dialogID string) {
 		p.removeAllObjects()
 		return
 	}
-	
+
 	for {
 		select {
 		case jsonCommand := <- p.commandChannel:
@@ -307,27 +292,4 @@ func (p *proxyInstance) processCommand(jsonCommand []byte, responseProducer chan
 	}
 	fmt.Printf("sendJSON is %s\n", string(sendJSON))
 	responseProducer <- sendJSON
-}
-
-// signalCatcher is a function to allows us to stop the application through an
-// operating system signal.
-func signalCatcher() {
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT)
-	sig := <-ch
-	log.Printf("Signal received: %v", sig)
-	os.Exit(0)
-}
-
-func main() {
-	// Setup a new Event producer and Command consumer for every application
-	// we've configured in the configuration file.
-	ari.InitBus(config.MessageBus, config.BusConfig)
-	for _, app := range config.Applications {
-		producer := ari.InitProducer(app)	// Initialize a new producer channel using the ari.IntProducer function.
-		go runEventHandler(app, producer)	// create new websocket connection for every application and pass the producer channel
-	}
-
-	go signalCatcher()	// listen for os signal to stop the application
-	select {}
 }
